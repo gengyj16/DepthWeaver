d1b6048d8cbd4218a015dbf429a4c51d1e440cb1
Eugene Geng <819514379m@gmail.com>
2025-08-11 10:17:04 +0000

    depth-anything-v2的GitHub链接换成 https://github.com/DepthAnything/Depth-Anyt

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -38,13 +38,13 @@ export default function AboutPage() {
                     <div className="space-y-2">
                         <h3 className="font-semibold">设计灵感</h3>
                         <p className="text-muted-foreground">
-                            灵感来源于iOS 17图库照片可生成空间照片的功能。此网页应用旨在通过开源技术，在浏览器中复现类似的沉浸式体验。
+                            灵感来源于iOS 26图库照片可生成空间照片的功能。此网页应用旨在通过开源技术，在浏览器中复现类似的沉浸式体验。
                         </p>
                     </div>
                     <div className="space-y-2">
                         <h3 className="font-semibold">实现技术</h3>
                         <p className="text-muted-foreground">
-                            通过强大的开源模型 <a href="https://github.com/LiheYoung/Depth-Anything" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">Depth-Anything-V2</a> 生成深度图，并结合 <a href="https://threejs.org/" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">Three.js</a> 在网页上构建和渲染3D场景，从而实现了“低配版”空间照片效果。
+                            通过强大的开源模型 <a href="https://github.com/DepthAnything/Depth-Anything-V2" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">Depth-Anything-V2</a> 生成深度图，并结合 <a href="https://threejs.org/" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">Three.js</a> 在网页上构建和渲染3D场景，从而实现了“低配版”空间照片效果。
                             此外，这个项目本身是 <a href="https://studio.firebase.google.com/" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">Firebase Studio</a> 构建的。
                         </p>
                     </div>
