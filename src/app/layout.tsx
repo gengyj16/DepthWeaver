e06b04fd54e77a424b2da53b7f343b88bc898dd2
Eugene Geng <819514379m@gmail.com>
2025-08-18 16:20:31 +0000

    如何告知浏览器这是暗色网页

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="zh-CN" className="dark">
       <head>
+        <meta name="color-scheme" content="dark" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
